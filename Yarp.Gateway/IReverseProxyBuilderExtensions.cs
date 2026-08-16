088934c4afa01f3be8ea16487b794e7fed3a28b2
quincy-tromp <quincy.tromp@hotmail.com>
2026-08-16 10:18:21 +0200

    Add response transformation

---
+++ Yarp.Gateway/IReverseProxyBuilderExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Yarp.ReverseProxy.Transforms;
+
+namespace Yarp.Gateway;
+
+public static class IReverseProxyBuilderExtensions
+{
+    extension(IReverseProxyBuilder proxyBuilder)
+    {
+        public IReverseProxyBuilder DoCustomResponseTransformation()
+        {
+            return proxyBuilder.AddTransforms(async context =>
+            {
+                context.AddResponseTransform(async responseContext =>
+                {
+                    var proxyResponse = responseContext.ProxyResponse;
+                    if (proxyResponse is null)
+                    {
+                        return;
+                    }
+
+                    var mediaType = proxyResponse.Content.Headers.ContentType?.MediaType;
+                    if (mediaType is null || !mediaType.Contains("application/json", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return;
+                    }
+
+                    var originalBytes = await proxyResponse.Content.ReadAsByteArrayAsync();
+                    if (originalBytes.Length == 0)
+                    {
+                        return;
+                    }
+
+                    try
+                    {
+                        var body = Encoding.UTF8.GetString(originalBytes);
+
+                        var node = JsonNode.Parse(body);
+
+                        if (node is not JsonObject obj)
+                        {
+                            return;
+                        }
+
+                        obj["servedBy"] = "gateway";
+
+                        var newBody = obj.ToJsonString();
+                        var newBytes = Encoding.UTF8.GetBytes(newBody);
+
+                        responseContext.SuppressResponseBody = true;
+
+                        responseContext.HttpContext.Response.ContentLength = newBytes.Length;
+
+                        await responseContext.HttpContext.Response.Body.WriteAsync(newBytes);
+                    }
+                    catch (JsonException)
+                    {
+                        responseContext.SuppressResponseBody = true;
+
+                        responseContext.HttpContext.Response.ContentLength = originalBytes.Length;
+
+                        await responseContext.HttpContext.Response.Body.WriteAsync(originalBytes);
+                    }
+                });
+            });
+        }
+    }
+}
--- Yarp.Gateway/Program.cs
+++ Yarp.Gateway/Program.cs
@@ -1,7 +1,14 @@
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Yarp.Gateway;
+using Yarp.ReverseProxy.Transforms;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .DoCustomResponseTransformation();
 
 var app = builder.Build();
 
