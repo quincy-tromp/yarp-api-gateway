50c059ae66a381b32d6a633aeaa21b82c8df2379
quincy-tromp <quincy.tromp@hotmail.com>
2026-08-16 17:52:23 +0200

    Add query parameter to get products endpoint

--- Product.Api/Program.cs
+++ Product.Api/Program.cs
@@ -15,9 +15,25 @@ var products = new Faker<Product>()
     .RuleFor(p => p.Stock, f => f.Random.Int(0, 100))
     .Generate(20);
 
-app.MapGet("/products", () =>
+app.MapGet("/products", (string? ids) =>
 {
-    return Results.Ok(new { Items = products });
+    if (string.IsNullOrEmpty(ids))
+    {
+        return Results.Ok(new { Items = products });
+    }
+    
+    var productIds = ids
+        .Split(',', StringSplitOptions.RemoveEmptyEntries)
+        .Select(id => int.TryParse(id, out var value) ? value : (int?)value)
+        .Where(id => id.HasValue)
+        .Select(id => id!.Value)
+        .ToList();
+
+    var results = products
+        .Where(p => productIds.Contains(p.Id))
+        .ToList();
+
+    return Results.Ok(new { Items = results });
 });
 
 app.MapGet("/products/{id:int}", (int id) =>
